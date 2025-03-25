3cfd5c2a6541cd1da1dcfb4be0c10991291c6069
mp97dev <michipase@gmail.com>
2025-03-25 18:13:19 +0100

    update

--- src/app.py
+++ src/app.py
@@ -36,8 +36,7 @@ def wait_for_element(driver, locator, timeout=60, condition=EC.presence_of_eleme
     :param condition: Expected condition (default: presence_of_element_located)
     :return: The WebElement if found, else raises TimeoutException
     """
-    print(driver.current_url)
-    print(driver.page_source)
+    logging.log(logging.CRITICAL, driver.page_source)
     return WebDriverWait(driver, timeout).until(condition(locator))
 
 def download_instagram(driver: webdriver.Chrome, data: dict):
