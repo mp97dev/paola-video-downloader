6f90c824f0dfbcdfe52fd150ccec6c2266845ff8
mp97dev <michipase@gmail.com>
2025-03-25 17:17:23 +0100

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "[II] update and upgrade"
+sudo apt update
+sudo apt install -y google-chrome-stable
+CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+curl -sS -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip -d /usr/local/bin/
+chromedriver --version
+
 # start virtual environment
 echo "[II] python init"
 
